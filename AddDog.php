2a2006c71f6d7e7123c38a24def6a6006819da1c
Aditi Joshi <aditi6301@gmail.com>
2021-05-22 16:37:21 +0530

    Adoption

---
+++ AddDog.php
@@ -0,0 +1,65 @@
+<?php
+include('LoginProcess.php');
+if(isset($_POST['Add_title']))
+{
+    echo "hi there";
+    $user_id= $_SESSION["user_id"];
+    $name=$_POST['name'];
+    $age=$_POST['age'];
+    $description=$_POST['description'];
+    $breed=$_POST['breed'];
+    $folder ="uploads/"; 
+
+    $uploads_dir = "uploads/";
+
+    echo $_FILES["image"]["name"]; 
+    echo $_FILES["image"]["size"];
+    echo $_FILES["image"]["type"];
+    $pname = $_FILES["image"]["name"]; 
+    $tname=$_FILES["image"]["tmp_name"];
+    $allowed=array('jpeg','png' ,'jpg',NULL);
+    $name = pathinfo($_FILES['image']['name'], PATHINFO_FILENAME);
+    $extension = pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
+  
+     $increment = 0; 
+     $pname = $name . '.' . $extension;
+     if(!in_array($extension,$allowed) ) 
+    
+     { 
+        $_SESSION['invalidImage']=1;
+        Header( 'Location: adoption.php');
+    //   echo "Sorry, only JPG, JPEG, PNG & GIF  files are allowed.";
+     
+     }
+     else{
+        //  move_uploaded_file( $_FILES['image'] ['tmp_name'], $path); 
+        while(is_file($uploads_dir.'/'.$pname)) {
+        $increment++;
+        $pname = $name . $increment . '.' . $extension;
+     }
+     move_uploaded_file($tname, $uploads_dir.'/'.$pname);
+
+    $sql=$conn->prepare("INSERT INTO `adoptionpup`(`pup_id`,`user_id`,`name`,`age`,`description`,`image`,`breed`) VALUES (NULL,'$user_id','$name','$age','$description','$pname','$breed')");
+    $result=$sql->execute() or die($conn->error);
+    if($result)
+    {
+        $_SESSION['titlesuccess']=1;
+        Header( 'Location: adoption.php');
+    }
+    else
+    {
+        $_SESSION['titlesuccess']=0;
+        Header( 'Location: adoption.php');
+    }
+    
+
+}
+
+       
+}
+
+
+
+
+
+?>
--- doggo_data.sql
+++ doggo_data.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 21, 2021 at 10:35 AM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.2
+-- Generation Time: May 22, 2021 at 12:27 PM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,21 @@ SET time_zone = "+00:00";
 --
 -- Database: `doggo_data`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `adoptionpup`
+--
+
+CREATE TABLE `adoptionpup` (
+  `user_id` int(11) DEFAULT NULL,
+  `pup_id` varchar(20) NOT NULL,
+  `name` varchar(30) DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `image` varchar(500) DEFAULT NULL,
+  `breed` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -41,11 +56,19 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `first_name`, `last_name`, `email`, `password`, `mobile`) VALUES
-(1, 'Shreya', 'Kedia', 'shreyakedia149@gmail.com', '$2y$10$jO9zUB7yDZFVQUsqMUIuHeeBpfkByM8SdIENCmgahiFAEYqeSln6W', 11111111);
+(1, 'Shreya', 'Kedia', 'shreyakedia149@gmail.com', '$2y$10$jO9zUB7yDZFVQUsqMUIuHeeBpfkByM8SdIENCmgahiFAEYqeSln6W', 11111111),
+(2, 'Radha', 'Mujumdar', 'radhamujumdar@gmail.com', '$2y$10$KQU7mA2sNjYb/yRj4iNcxOfEpaUJvPd6rxCgkoHM8NmT2mDno6YBe', 7420827617);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `adoptionpup`
+--
+ALTER TABLE `adoptionpup`
+  ADD PRIMARY KEY (`pup_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `users`
@@ -61,7 +84,17 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `adoptionpup`
+--
+ALTER TABLE `adoptionpup`
+  ADD CONSTRAINT `adoptionpup_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
